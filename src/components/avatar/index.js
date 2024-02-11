f594fceff30db476f429ea0f3e9dc1372f7d7497
Chanchal Kumar <rrabshrivastava@gmail.com>
2024-02-11 17:44:55 +0530

    online offline css corrected

--- src/components/avatar/index.js
+++ src/components/avatar/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import OnlineIcon from "../../images/online.png";
 import OfflineIcon from "../../images/offline.png";
+import './styles.css';
+
 const Avator = ({ name, width=20, available=false }) => {
     return (
             <div>
                 <img src={`https://ui-avatars.com/api/?name=${name.split(' ').join('+')}&background=random`} alt={name} width={width} style={{borderRadius: '50%'}}  />
-                <span className="avator-available">
+                <span className="avator-available" >
                   { available ?  <img src={OnlineIcon} width={'10px'}  /> : <img src={OfflineIcon} width={'10px'} /> }
                     </span>
             </div>
